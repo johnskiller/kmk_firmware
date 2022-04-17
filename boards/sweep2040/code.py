bd2617442c0846301740e39e34d0f91bfb6082c3
John <john@skykiller.com>
2022-04-17 14:01:39 +0800

    keymap works well

--- boards/sweep2040/code.py
+++ boards/sweep2040/code.py
@@ -9,12 +9,10 @@ from kmk.modules.split import Split, SplitSide, SplitType
 from kmk.modules.encoder import EncoderHandler
 from kmk.modules.layers import Layers
 from kmk.modules.modtap import ModTap
+from kmk.modules.combos import Combos, Chord, Sequence
 from kmk.modules.tapdance import TapDance
 from kmk.modules.mouse_keys import MouseKeys
 #from kmk.extensions.lcd import LCD
-#keyboard.col_pins = (board.GP0)    # try D5 on Feather, keeboar
-#keyboard.row_pins = (board.GP1)    # try D6 on Feather, keeboar
-#keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 #LOWER = KC.MO(1)
 #RAISE = KC.MO(2)
@@ -31,38 +29,69 @@ keyboard = kb.KMKKeyboard()
 keyboard.debug_enabled=True
 keyboard.modules = [Layers(),ModTap(),TapDance(),MouseKeys()]
 
-ESC_LCTL = KC.MT(KC.ESC, KC.LCTL)
-ENT_LALT = KC.MT(KC.ENT, KC.LALT)
-FUN=5 # Function Keys
-SYM=4 # Symbol Layer
-MK =3 # Mouse key
-NAV=2 # Navication
-DIG=1 # Digits
+combos = Combos()
+keyboard.modules.append(combos)
 
+FUN=7 # Function Keys
+SYM=6 # Symbol Layer
+MK =4 # Mouse key
+NAV=3 # Navication
+DIG=2 # Digits
+QWERTY=1
+DVORAK=0
+
+TAB_LCTL = KC.MT(KC.TAB, KC.LCTL)
+ENT_LALT = KC.MT(KC.ENT, KC.LALT)
+SLSH_RSFT = KC.MT(KC.SLSH, KC.RSFT)
+#ESC_GRV=KC.TD(KC.ESC,KC.TILD,KC.GRV)
+NAV_SPC = KC.LT(NAV,KC.SPACE)
+
+combos.combos = [
+    Chord((KC.J, KC.K), KC.TG(MK))
+]
 keyboard.keymap = [
+    # DVORAK
+    # ,-----------------------------------------.                    ,-----------------------------------------.
+    # | Esc  |   '  |   ,  |   .  |   P  |   Y  |                    |   F  |   G  |   C  |   R  |   L  | BKSP |
+    # |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+    # | Tab  |   A  |   O  |   E  |   U  |   I  |                    |   D  |   H  |   T  |   N  |   S  |  ENT |
+    # |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+    # | Shft |   ;  |   Q  |   J  |   K  |   X  |-------.    ,-------|   B  |   M  |   W  |   V  |   Z  |   /  |
+    # `-----------------------------------------/       /     \      \-----------------------------------------'
+    #                          | LALT | LGUI | /       /       \      \  |  Ctl |  Up  |
+    #                          |      |      |/       /         \      \ |      |      |
+    #                          `---------------------'           '------''-------------'
+    #
+    [
+        # DVORAK
+        KC.GESC,  KC.QUOT, KC.COMM, KC.DOT,  KC.P,    KC.Y,                           KC.F,    KC.G,    KC.C,    KC.R,    KC.L,    KC.BSPC, \
+       TAB_LCTL,  KC.A,    KC.LT(SYM,KC.O),  KC.LT(DIG,KC.E),KC.LT(FUN,KC.U), KC.I,   KC.D,    KC.H,    KC.T,    KC.N,    KC.S,    KC.ENT, \
+        KC.LSFT,  KC.SCLN, KC.Q,    KC.J,    KC.K,    KC.X,                           KC.B,    KC.M,    KC.W,    KC.V,    KC.Z,    KC.SLSH, \
+                                        KC.LGUI, ENT_LALT,                          NAV_SPC, KC.BKDL,
+    ],
     [  #Layer 0 QWERTY
-        KC.TAB, KC.Q,   KC.W,   KC.E,   KC.R,  KC.T,                         KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,  KC.BSPC,\
-       ESC_LCTL,KC.A,  KC.LT(SYM,KC.S),   KC.LT(1,KC.D),KC.LT(FUN,KC.F),  KC.G,        KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,\
-        KC.LSFT,KC.Z,   KC.X,   KC.C,   KC.V,  KC.B,                         KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.RSFT,\
-                                     KC.TD(KC.LGUI,KC.TG(MK)),   ENT_LALT,                  KC.LT(NAV,KC.SPACE),   KC.BSPC,
+       KC.GESC, KC.Q,   KC.W,   KC.E,   KC.R,  KC.T,                                    KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,  KC.BSPC,\
+       TAB_LCTL,KC.A,  KC.LT(SYM,KC.S),   KC.LT(DIG,KC.D),KC.LT(FUN,KC.F),  KC.G,       KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,\
+        KC.LSFT,KC.Z,   KC.X,   KC.C,   KC.V,  KC.B,                                    KC.N,    KC.M, KC.COMM,  KC.DOT, SLSH_RSFT, KC.RSFT,\
+                                     KC.LGUI,   ENT_LALT,                    NAV_SPC,   KC.BKDL,
     ],
     [   #Layer 1 Digit
         KC.GRV ,_______,_______,_______,_______,_______,                         KC.PLUS,  KC.N7,    KC.N8,    KC.N9,   KC.EQL,  KC.BSPC,\
-        _______,_______,_______,XXXXXXX,_______,_______,                         KC.MINUS, KC.N4,    KC.N5,    KC.N6,   KC.ASTR, KC.QUOT,\
+        _______,_______,_______,XXXXXXX,KC.UNDS,_______,                         KC.MINUS, KC.N4,    KC.N5,    KC.N6,   KC.ASTR, KC.QUOT,\
         _______,_______,_______,_______,_______,_______,                         KC.N0,    KC.N1,    KC.N2,    KC.N3,   _______, KC.RSFT,\
-                                            KC.LGUI,   KC.ENT,                  KC.SPACE,   KC.BSPC,
+                                        _______,_______,                        KC.DOT,   _______,
     ],
     [   #Layer 2 navication
         KC.GRV ,KC.EXLM,KC.AT,  KC.HASH,KC.DLR, KC.PERC,                         KC.HOME,KC.END ,KC.PGUP,KC.PGDN,_______,_______,\
         _______,KC.CIRC,KC.AMPR,KC.ASTR,KC.LPRN,KC.RPRN,                         KC.LEFT,KC.DOWN,KC.UP,  KC.RGHT,_______,_______,\
-        _______,_______,_______,_______,_______,_______,                         _______,_______,_______,_______,_______,_______,\
-                                            KC.LGUI,   KC.ENT,                  KC.NO,   KC.BSPC,
+        _______,_______,_______,_______,_______,_______,                         KC.DF(1),_______,_______,_______,_______,_______,\
+                                        _______,_______,                         KC.NO,   KC.DF(0),
     ],
     [   #Layer 3 Mouse key
         _______,_______,KC.MB_RMB,KC.MS_UP,KC.MB_LMB,KC.MW_UP,                   _______,KC.MB_LMB,KC.MB_MMB,KC.MB_RMB, _______,_______,\
-        _______,_______,KC.MS_LT,KC.MS_DN,KC.MS_RT,KC.MW_DN,                     _______,_______, _______, _______,_______,_______,\
+        _______,_______,KC.MS_LT,KC.MS_DN,KC.MS_RT,KC.MW_DN,                     KC.MS_LT,KC.MS_DN,KC.MS_UP,KC.MS_RT,_______,_______,\
         _______,_______,_______,_______,_______,_______,                         _______,_______, _______, _______,_______,_______,\
-                                            KC.MB_MMB,   KC.ENT,                  KC.SPACE,   KC.TO(0),
+                                            KC.MB_MMB,   KC.ENT,                  KC.SPACE,   KC.TG(MK),
     ],
     [   #Layer 4 Symbols
         _______,_______,_______,_______,_______,_______,                         KC.HASH,KC.LPRN, KC.RPRN, KC.LBRC,KC.RBRC,_______,\
@@ -74,6 +103,12 @@ keyboard.keymap = [
         _______,_______,_______,_______,_______,_______,                         KC.F1  ,KC.F2  , KC.F3  , KC.F4  ,KC.F5  ,_______ ,\
         _______,_______,_______,_______,XXXXXXX,_______,                         KC.F6  ,KC.F7  , KC.F8  , KC.F9  ,KC.F10 ,_______ ,\
         _______,_______,_______,_______,_______,_______,                         KC.F11 ,KC.F12 , KC.F13 , KC.F14 ,KC.F15 ,_______ ,\
+                                            KC.LGUI,   KC.LALT,                  KC.SPACE,   KC.BSPC,
+    ],
+    [   #Layer 6 Digit2
+        _______,KC.N1,  KC.N2,  KC.N3,  KC.N4,  KC.N5,                           _______,_______, _______, _______,_______,_______,\
+        _______,KC.N6,  KC.N7,  KC.N8,  KC.N9,  KC.N0,                           _______,_______, _______, _______,_______,_______,\
+        _______,_______,_______,_______,_______,_______,                         _______,_______, _______, _______,_______,_______,\
                                             KC.LGUI,   KC.ENT,                  KC.SPACE,   KC.BSPC,
     ],
     [   #Layer 1 Digit
@@ -92,6 +127,7 @@ split = Split(split_target_left=False,split_offset=20,use_pio=True,
 debug_enabled=True,)
 
 split.data_pin=board.GP16
+split.data_pin2=board.GP17
 
 encoder_handler = EncoderHandler()
 keyboard.modules.append(split)
--- boards/sweep2040/kb.py
+++ boards/sweep2040/kb.py
@@ -8,13 +8,23 @@ _KEY_CFG = [board.GP10, board.GP4, board.GP3,  board.GP0, board.GP1,  board.GP2,
                                                           board.GP15, board.GP18
             ]
 
-class KMKKeyboard(_KMKKeyboard):
-    def __init__(self):
-        self.matrix = KeysScanner(_KEY_CFG)
 
-        self.matrix.coord_mapping = [
+def six_col_map():
+    return [
              0, 1, 2, 3, 4, 5,     25,24,23,22,21,20,
              6, 7, 8, 9,10,11,     31,30,29,28,27,26,
             12,13,14,15,16,17,     37,36,35,34,33,32,
                         18,19,     39,38,
-        ] 
+        ]
+def five_col_map():
+    return [
+             0, 1, 2, 3, 4, 5,     24,23,22,21,20,
+             6, 7, 8, 9,10,11,     29,28,27,26,25,
+            12,13,14,15,16,17,     34,33,32,31,30,
+                        18,19,     36,35,
+        ]
+class KMKKeyboard(_KMKKeyboard):
+    def __init__(self):
+        self.matrix = KeysScanner(_KEY_CFG)
+
+        self.coord_mapping = six_col_map()
